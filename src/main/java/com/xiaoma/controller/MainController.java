8a80ec580a85c282a1371259241a487f242cfe14
xiaoma <3259332518@qq.com>
2018-09-15 01:20:00 +0800

    首页文章添加

--- src/main/java/com/xiaoma/controller/MainController.java
+++ src/main/java/com/xiaoma/controller/MainController.java
@@ -1,17 +1,27 @@
 package com.xiaoma.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.xiaoma.entity.ARTICLE;
+import com.xiaoma.service.ArticleService;
 @Controller
 public class MainController {
-	
-	
+	@Autowired()
+	private  ArticleService articleService;
 	
 	@RequestMapping("/index")
-	public String index(Model m)
+	public String index(Model m,@RequestParam(name="pages",defaultValue="0")int page,
+			@RequestParam(name="size",defaultValue="10")int size)
 	{
 		//导入文章列表
+		String ISSHOW="1";
+		Page<ARTICLE> article=articleService.findARTICLECriteria(page, size, ISSHOW);
+		m.addAttribute("articles",article);
 		return "thymeleaf/index";
 	}
 	@RequestMapping("/about")
